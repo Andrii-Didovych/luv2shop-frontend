e106e75d490dda38e2150dbbad96d14670b01a36
Andrii-Didovych <andrii.didovych@sombrainc.com>
2020-03-14 13:12:47 +0200

    init commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  id: number;
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  active: boolean;
+  unitsInStock: boolean;
+  dateCreated: boolean;
+  lastUpdated: boolean;
+  imageUrl: string;
+}
---
+++ src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from '../../common/product';
+import {ProductService} from '../../services/product.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit {
+
+  products: Product[];
+  currentCategoryId: number;
+  currentCategoryName: string;
+  searchMode: boolean;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+        this.listProducts();
+      }
+    );
+  }
+
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+  private handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+  handleListProducts() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
+    } else {
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+}
